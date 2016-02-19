a26e9c3ffbd6e438ed29db177a0e20e79300a4c9
YongHao Hu <christopherwuy@gmail.com>
2016-02-19 21:26:19 +0800

    TCPDump: Add demo.

--- GT.py
+++ GT.py
@@ -23,8 +23,8 @@ def analyse_lsof(file_name):
     file = open(file_name)
     i = 0
     while 1:
-    #    if i > 10:
-    #        break
+        #    if i > 10:
+        #        break
         i = i+1
         line = file.readline()
         if not line:
@@ -53,4 +53,41 @@ def analyse_lsof(file_name):
             time_stamp = line
             print "time_stamp ", time_stamp
 
-analyse_lsof("input.txt")
+def my_re_of_first_line(first_line):
+    m = re.match(r'(\d+.\d+)', first_line)
+    if m:
+        print 'ok'
+        print "****group(0): \n", m.groups(0)
+        #print "****group(x): \n", m.groups(1)[1:4]
+    else:
+        print 'failed'
+
+def my_re_of_second_line(second_line):
+    second_line = re.sub("\s+",'',second_line)
+    m = re.match(r'(\d+.\d+.\d+.\d+).(\d+)>(\d+.\d+.\d+.\d+).(\w+):.*?\[(.*?)\]', second_line)
+    if m:
+        print 'my_re_of_second_line: ok'
+        print "my_re_of_second_line: \n", m.groups(0)
+        print "****group(x): \n", m.groups(1)[1:4]
+    else:
+        print 'my_re_of_second_line: failed'
+
+def analyse_tcpdump(file_name):
+    file = open(file_name)
+    i = 0
+    while 1:
+        line = file.readline()
+        i = i+1
+        if not line:
+            break
+        #if i > 10:
+        #    break
+        if i%2 != 0:
+            my_re_of_first_line(line)
+        else:
+            my_re_of_second_line(line)
+
+
+#analyse_lsof("lsof.txt")
+analyse_tcpdump("tcp.txt")
+
